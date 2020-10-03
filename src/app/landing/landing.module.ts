15c9ccd3491b28ebe74cb0b1e40a4a7711dd37bd
Zee Badr <zeeshan.badr@mail.utoronto.ca>
2020-10-03 19:51:34 -0400

    removed not working code

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -4,17 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
-import { FolderPageRoutingModule } from './map-routing.module';
-
-import { FolderPage } from './map.page';
-
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    LandingModule
   ],
-  declarations: [FolderPage]
+  declarations: [LandingPage]
 })
-export class FolderPageModule { }
+export class LandingModule { }
